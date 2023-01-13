e38b5a75f2cef76bca85127c08a1b5d9f946cfcc
Tola Awofolu <oawofolu@pivotal.io>
2023-01-13 00:54:57 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'mlflow==2.0.1',
         'pandas',
-        'https://files.pythonhosted.org/packages/ab/88/dc597fdadd74b1a2fe7d4e4fb56f44a2b444a01b76ec35f31c7eee28f2d0/ray-2.1.0-cp310-cp310-manylinux2014_x86_64.whl',
+        'ray @ https://files.pythonhosted.org/packages/ab/88/dc597fdadd74b1a2fe7d4e4fb56f44a2b444a01b76ec35f31c7eee28f2d0/ray-2.1.0-cp310-cp310-manylinux2014_x86_64.whl',
         'wheel'
     ]
 )
